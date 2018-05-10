eb29a619893fbb7b9bb0f6bfe8095d7da5f92a5f
Flamy Hacker <hackflamy@outlook.com>
2018-05-10 17:03:26 +0200

    updated admin controller

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -19,6 +19,24 @@
                     echo "user created";
                 }
             }
+            if(isset($_POST['btnRemove'])){
+                $username = filter_input(INPUT_POST,'username')? filter_input(INPUT_POST,'username'):0;
+                if($username != 0){
+                    Admin::removeUser($username);
+                    require_once('view/admin/createuser.php');
+                    echo "user removed";
+                }
+            }
+            if(isset($_POST['btnReset'])){
+                $username = filter_input(INPUT_POST,'username')?filter_input(INPUT_POST,'username'):0;
+                $password = filter_input(INPUT_POST,'password')?filter_input(INPUT_POST,'password'):0;
+                if($username != 0 || $password !=0){
+                    Admin::resetPassword($username,$password);
+                    require_once('view/admin/createuser.php');
+                    echo "password reseted";
+                }
+
+            }
             
         }
     }
