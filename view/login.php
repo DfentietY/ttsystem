834477ad03b7d3a353e0945196f45a6086c52baf
unknown <mphlt112@gmail.com>
2018-05-12 13:13:36 +0200

    Fixed Font problem

--- view/login.php
+++ view/login.php
@@ -4,9 +4,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="font/stylesheet.css"></script>
         <script src="js/jquery-3.3.1.min.js"></script>
         <script src="bootstrap/js/bootstrap.js"></script>
-        <script src="font/stylesheet.css"></script>
         <script src="js/admin.js"></script>
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script>
