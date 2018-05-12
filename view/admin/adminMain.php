8bf89b321fef3b0842a2588133dda9c0672fa6e8
unknown <mphlt112@gmail.com>
2018-05-12 12:01:20 +0200

    Follow up updates

--- view/admin/adminMain.php
+++ view/admin/adminMain.php
@@ -107,16 +107,8 @@
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
                 <div class="modal-body">
-                    <table border=1>
-                        <thead>
-                            <th>Leccturer Name</th>
-                            <th>Lecturer Username</th>
-                        </thead>
-                        <tbody>
-                            <div id="lectNames">
-                            </div>
-                        </tbody>
-                    </table>
+                    <div id="lectNames">
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -124,6 +116,11 @@
             </div>
         </div>
     </div>
+    <script>        
+        $("#btnViewModal").on("click", function (){
+            $("#lectNames").load("?controller=admin&action=viewLecturer");
+        });
+    </script>
     <?php 
         require_once('route.php');
     ?>
--- view/admin/getLecturerUsernames.php
+++ view/admin/getLecturerUsernames.php
@@ -1,12 +1,20 @@
 <?php 
-    require_once('model/admin.php');
-    $list = Admin::viewLecturer();
     if(isset($list)){
+        echo "<table class='table table-striped table-hover'>
+                <thead>
+                    <tr>
+                        <th>Lecturer Name</th>
+                        <th>Lecturer Username</th>
+                    </tr>
+                <thead>
+                <tbody>";
         foreach($list as $lecturer){
             echo "<tr>
                 <td>$lecturer->name</td>
                 <td>$lecturer->username</td>
             </tr>";
         }
+        echo "  </tbody>
+              </table>";
     }
 ?>
