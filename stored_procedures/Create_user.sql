c1f22226ef54893e3da1319cf80d43a6692c983b
unknown <malepesuprise@gmail.com>
2018-05-12 12:26:57 +0200

    stored procedure create user

---
+++ stored_procedures/Create_user.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE PROCEDURE CRE_USER(p_username IN VARCHAR2, p_password IN VARCHAR2, p_role IN VARCHAR2, p_message OUT VARCHAR2)
+IS
+BEGIN
+EXECUTE IMMEDIATE 'CREATE USER '||p_username||' IDENTFIED BY '||p_password;
+IF p_role = 'Lecturer' THEN
+EXECUTE IMMEDIATE 'GRANT Lecturer TO '||p_username;
+ELSIF p_role = 'Student' THEN
+EXECUTE IMMEDIATE 'GRANT Student TO '||p_username;
+ELSE
+p_message := 'Role unknown';
+END IF;
+END;
