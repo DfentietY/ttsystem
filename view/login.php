3fcd7590ebfc0f04ae1363dc1d90f3fcec0fd93d
unknown <hackflamy@outlook.com>
2018-05-12 11:32:31 +0200

    updated view

--- view/login.php
+++ view/login.php
@@ -3,6 +3,11 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <script src="js/jquery-3.3.1.min.js"></script>
+        <script src="bootstrap/js/bootstrap.js"></script>
+        <script src="font/stylesheet.css"></script>
+        <script src="js/admin.js"></script>
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script>
             function close_window() {
@@ -16,28 +21,25 @@
         <title>Welcome</title>
     </head>
     <body>
+    <div class="container centered text-center">
         <form method="POST">
                  <!--Inputs for username and pasword -->
             <div class="container">
-                    <label for="uname"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="username" required />
-                    <label for="pwd"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="password" required/>
-                    <input type="submit" class="logbtn" name="btnLogin" value="Login">
-                    <label>
-                        <input type="checkbox" checked="checked" name="remember"> Remember me
-                    </label>
+                    <label style="font-family: 'varela_roundregular';font-size: 20px;" for="uname"><b>Username</b></label>
+                    <input type="text" class="form-control" placeholder="Enter Username" name="username" required />
+                    <label style="font-family: 'varela_roundregular';font-size: 20px;" for="pwd"><b>Password</b></label>
+                    <input type="password" class="form-control" placeholder="Enter Password" name="password" required/>
+                    </br>
+                    <input type="submit" class="btn btn-success" name="btnLogin" value="Login">
                     <input type="hidden" name="controller" value="main" />
                     <input type="hidden" name="action" value="login" />
             </div>
+            </br>
             <?php 
                 require_once('route.php');
             ?>
-                <!--Buttons for forgot pwd and exit -->
-            <div class="container" style="background-color:#f1f1f1">
-                <input type="submit" class="cancelbtn" name="Exit" onclick="close_window(); return false" value="Exit">
-                <span class="pwd">Forgot <a href="#">password?</a></span>
-            </div>
+       
         </form>  
+        </div>
     </body>
 </html>
