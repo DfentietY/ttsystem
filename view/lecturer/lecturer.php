92b623be3782f1f1f2dd9527015d2c04c8dabe5b
unknown <hackflamy@outlook.com>
2018-05-14 13:20:49 +0200

    fixed lacture view

--- view/lecturer/lecturer.php
+++ view/lecturer/lecturer.php
@@ -4,15 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="bootstrap/js/bootstrap.js"></script>
     <script src="js/lecturer.js"></script>
     <title>Welcome</title>
     <style>
-        body {
-            padding-top: 3.6em;
-        }
         #nav-menu {
             margin-right: 1rem; 
         }
@@ -24,26 +22,36 @@
     </style>
 </head>
 <body>
-    <div class="container">
-        <nav class="navbar navbar-default">
+<nav class="navbar navbar-expand-lg navbar-dark bg-black">
+<a class="pull-left" href="#"><img src="img/tut-logo-blue.jpg"/></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Profile <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">View Lecture</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Log Out</button>
+    </form>
+  </div>
+</nav>
+ 
+        <!--<nav class="navbar navbar-default">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <img src="img/tut-logo-blue.jpg" class="navbar-brand" />
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-menu" aria-expanded="false">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                    <a class="pull-left" href="#"><img src="img/tut-logo-blue.jpg"/></a>
                 </div>
-           
-                <ul class="nav navbar-nav">
-                    <li><a id="profile" href="#">Profile</a></li>
-                    <li><a id="subjects" href="#">View Subjects</a></li>
-                </ul>
-        
-            </div>
-        </nav>
-    </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Profile</a></li>
+                        <li><a href="#">view lecture</a></li>
+                         </ul>
+        </nav>-->
+
 </body>
 </html>
