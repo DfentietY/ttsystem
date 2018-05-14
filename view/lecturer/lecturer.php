bcac4656662a4c78be0e02f0a1ab3e215e31ebde
unknown <mphlt112@gmail.com>
2018-05-14 13:25:20 +0200

    fixed format

--- view/lecturer/lecturer.php
+++ view/lecturer/lecturer.php
@@ -22,36 +22,27 @@
     </style>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-black">
-<a class="pull-left" href="#"><img src="img/tut-logo-blue.jpg"/></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Profile <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">View Lecture</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Log Out</button>
-    </form>
-  </div>
-</nav>
- 
-        <!--<nav class="navbar navbar-default">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a class="pull-left" href="#"><img src="img/tut-logo-blue.jpg"/></a>
-                </div>
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Profile</a></li>
-                        <li><a href="#">view lecture</a></li>
-                         </ul>
-        </nav>-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-black">
+        <a class="pull-left" href="#"><img src="img/tut-logo-blue.jpg"/></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Profile <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">View Lecture</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Log Out</button>
+            </form>
+        </div>
+    </nav>
+    <div class="container-fluid" id="form">
 
+    </div>
 </body>
 </html>
