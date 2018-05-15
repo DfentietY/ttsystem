af7ad11bf8dd8e87bd30038cf9ba1f56eda6b4ee
unknown <mphlt112@gmail.com>
2018-05-15 13:32:30 +0200

    added error display

--- model/alert.php
+++ model/alert.php
@@ -13,7 +13,13 @@ class Alert{
             }
         }
         public function alertError($message){
-
+            echo '<div class="alert alert-danger" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h3>Connection Error</h3>
+                    <p>'.$message.'</p>
+                </div>';
         } 
 
         public function alertSuccess($message){
