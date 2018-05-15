f3d9fd83a253ae179f9d012a2d94a25ebe90bd5f
unknown <mphlt112@gmail.com>
2018-05-15 11:31:44 +0200

    Updated functionality

--- controller/lecturer_controller.php
+++ controller/lecturer_controller.php
@@ -31,17 +31,18 @@
                             <div class="form-group">
                                 <label for="lastname">Lastname: </label>
                                 <input type="text" id="lastname" class="form-control" readonly value="'.$list[0]->lastname.'" />
-                                <label for="initials"><Initials: </label>
+                                <label for="initials">Initials: </label>
                                 <input type="text" id="initials" class="form-control" readonly value="'.$list[0]->initials.'" />
-                                <label for="password">Password</label>
+                                <label for="password">Password: </label>
                                 <input type="password" class="form-control" placeholder="Password" name="password" id="password" value="" />
-                                <label for="confPass">Confirm Password</label>
+                                <label for="confPass">Confirm Password: </label>
                                 <input type="password" class="form-control" placeholder="Confirm Password" id="confPass" value="" />
-                                <label for="email">Email Address</label>
+                                <label for="email">Email Address: </label>
                                 <input type="email" class="form-control" name="email" id="email" value="'.$list[0]->email.'" />
-                                <label for="birthday">Birthday</label>
+                                <label for="birthday">Birth Date: </label>
                                 <input type="date" class="form-control" name="birthday" id="birthday" value="'.$list[0]->birthday.'" />
-                                <input type="submit" class="btn btn-default" name="btnUpdLect" value="Update Information" />
+                                <br />
+                                <input type="submit" class="btn btn-default" name="btnProf" value="Update Information" />
                             </div>
                         </form>
                     </div>';
--- view/lecturer/lecturer.php
+++ view/lecturer/lecturer.php
@@ -5,28 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="font/stylesheet.css" />
+    <link rel="stylesheet" href="css/lecturer.css" />
     <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="bootstrap/js/bootstrap.js"></script>
     <script src="js/lecturer.js"></script>
     <title>Welcome</title>
-    <style>
-        #nav-menu {
-            margin-right: 1rem; 
-        }
-        img{
-            filter: invert(100%);
-            width: 150px;
-            height: 75px;
-        }
-        .navbar-nav > li{
-            padding-left: 1rem;
-            padding-right: 1rem;
-        }
-        .center-form {
-            width: 25rem;
-        }
-    </style>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -41,11 +25,13 @@
                     </button>
                 </li>
                 <li class="nav-item">
-                    <button class="nav-link btn btn-outline-info my-2 my-sm-0" id="subjects" onclick="subjectFill()">View Lecture</button>
+                    <button class="nav-link btn btn-outline-info my-2 my-sm-0" id="subjects" onclick="subjectFill()">View Subjects</button>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Log Out</button>
+            <form class="form-inline my-2 my-lg-0" method="POST">
+                <button class="btn btn-outline-danger my-2 my-sm-0" name="logout" type="submit">Log Out</button>
+                <input type="hidden" name="controller" value="main" />
+                <input type="hidden" name="action" value="logout" />
             </form>
         </div>
     </nav>
