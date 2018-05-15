83550bd6e6bed13a3447014d5babbd672cb2dba3
unknown <mphlt112@gmail.com>
2018-05-15 13:37:54 +0200

    added error display

--- model/alert.php
+++ model/alert.php
@@ -6,29 +6,27 @@ class Alert{
         }
 
         public static function alertMessage($message) {
-            echo '<script> console.log("Alert happens");</script>';
             if($message[1] == 'TT512'){
-                alertError($message[0]);
+                Alert::alertError($message[0]);
             }else if($message[1] == 'TT132'){
-                alertSuccess($message[0]);
+                Alert::alertSuccess($message[0]);
             }
         }
-        public function alertError($message){
+        public static function alertError($message){
+
             echo '<div class="alert alert-danger" role="alert">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
-                    <h3>Connection Error</h3>
                     <p>'.$message.'</p>
                 </div>';
         } 
 
-        public function alertSuccess($message){
+        public static function alertSuccess($message){
             echo '<div class="alert alert-success" role="alert">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
-                    <h3>Connection Error</h3>
                     <p>'.$message.'</p>
                 </div>';
         }
