2fa3872cd27db1c8907d691c7ff7ecf79c589632
unknown <mphlt112@gmail.com>
2018-05-15 11:11:42 +0200

    lecturer javascript file issue fixed

--- js/lecturer.js
+++ js/lecturer.js
@@ -1,9 +1,29 @@
-$(document).ready(function () {
-    $("#profile").on('click', function() {
-        $("#form").load("?controller=lecturer&admin=maintainProf");
-    });
+(function() {
+    console.log('ajax works');
+}());
 
-    $("#subjects").on('click', function() {
-        $("#form").load("?controller=lecturer&admin=viewSubjects");
-    });
-});
+function AjaxReady() {
+    var form = document.getElementById('form');
+    var xmlhttp;
+    
+    if(window.XMLHttpRequest){ xmlhttp = new XMLHttpRequest(); }
+    else{ xmlhttp = new ActiveXObject("Microsoft.XMLHttp"); }
+    xmlhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            form.innerHTML = this.responseText;
+        }
+    }
+    return xmlhttp;
+}
+
+function profileFill() {
+    xmlhttp = AjaxReady();
+    xmlhttp.open("GET", "?controller=lecturer&action=maintainProf");
+    xmlhttp.send();
+}
+
+function subjectFill() {
+    xmlhttp = AjaxReady();
+    xmlhttp.open("GET", "?controller=lecturer&action=viewSubjects");
+    xmlhttp.send();
+}
