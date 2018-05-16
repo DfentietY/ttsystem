efe5320586450bb54c85f494d1935f19bbf76f34
unknown <mphlt112@gmail.com>
2018-05-16 15:54:08 +0200

    Added insert stored procedure

---
+++ stored_procedures/insert_lecture.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE PROCEDURE insert_lecture(p_username IN VARCHAR2, 
+                                            p_lastname IN VARCHAR2, 
+                                            p_initials IN VARCHAR2, 
+                                            p_password IN VARCHAR2)
+IS
+BEGIN
+    INSERT INTO tbllecturer()
+    VALUES();
+END;
+/
