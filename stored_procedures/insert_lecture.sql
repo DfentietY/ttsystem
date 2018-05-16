dcab3356b6449b5383115881a3bc490cd0aa35e1
unknown <mphlt112@gmail.com>
2018-05-16 18:05:20 +0200

    Fixed issues

--- stored_procedures/insert_lecture.sql
+++ stored_procedures/insert_lecture.sql
@@ -4,7 +4,13 @@ CREATE OR REPLACE PROCEDURE insert_lecture(p_username IN VARCHAR2,
                                             p_password IN VARCHAR2)
 IS
 BEGIN
-    INSERT INTO tbllecturer()
-    VALUES();
+    INSERT INTO tbllecturer(lect_lastname, lect_initials, lect_username, lect_password)
+    VALUES(p_lastname, p_initials, p_username, p_password);
+    p_message := 'Lecture inserted Successfully';
+    p_code := 'TT132';
+EXCEPTION 
+    WHEN others THEN
+        p_message := 'Lecture could not be inserted into the database';
+        p_code := 'TT512';
 END;
 /
