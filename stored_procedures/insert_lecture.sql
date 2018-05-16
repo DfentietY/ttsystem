d2733943f9da5485453ff1a1d53f3e9bc3877a07
unknown <mphlt112@gmail.com>
2018-05-16 18:14:28 +0200

    Fixed issues

--- stored_procedures/insert_lecture.sql
+++ stored_procedures/insert_lecture.sql
@@ -1,7 +1,9 @@
 CREATE OR REPLACE PROCEDURE insert_lecture(p_username IN VARCHAR2, 
                                             p_lastname IN VARCHAR2, 
                                             p_initials IN VARCHAR2, 
-                                            p_password IN VARCHAR2)
+                                            p_password IN VARCHAR2,
+                                            p_message OUT VARCHAR2,
+                                            p_code OUT VARCHAR2)
 IS
 BEGIN
     INSERT INTO tbllecturer(lect_lastname, lect_initials, lect_username, lect_password)
