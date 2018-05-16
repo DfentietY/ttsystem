bdd818e6b89da3c0dce46e21452a3689f97d2764
unknown <mphlt112@gmail.com>
2018-05-16 14:02:54 +0200

    student ui page formatted

--- view/student/getStudentUi.php
+++ view/student/getStudentUi.php
@@ -1,11 +1,13 @@
 <?php
-echo ' <div class="center-form" > <form method=GET>
-        <input placeholder="Enter Old Password" class="form-control" type="password" name="oldpassword" />
-        <input placeholder="Enter New Password" class="form-control" type="password" name="newpassword" />
-        <input placeholder="Enter Confirm Password" class="form-control" type="password" name="conpassword" />
-        <p><input type="hidden" name="controller" value="student"></p>
-        <p><input type="hidden" name="action" value="maintainProf"></p>
-        <input type="submit" class="btn btn-def btn-block" name="btnChange" value="Change Password">
-      </form> </div>
+echo ' <div class="center-form" > 
+        <form method="POST">
+          <input placeholder="Enter Old Password" class="form-control" type="password" id="oldPass" name="oldpassword" />
+          <input placeholder="Enter New Password" class="form-control" type="password" id="newPass" name="newpassword" />
+          <input placeholder="Enter Confirm Password" class="form-control" type="password" id="conPass" />
+          <p><input type="hidden" name="controller" value="student"></p>
+          <p><input type="hidden" name="action" value="maintainProf"></p>
+          <input type="submit" class="btn btn-def btn-block" name="btnChange" value="Change Password">
+        </form> 
+      </div>
     ';
 ?>
